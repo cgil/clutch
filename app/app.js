82aca516d748e943796abdb85c44d49ac4849edc
Carlos Gil <carlos@uber.com>
2016-05-21 19:20:03 +0100

    Updated store

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {TabsPage} from './pages/tabs/tabs';
+import {StorePage} from './pages/store/store';
 
 
 @App({
@@ -13,7 +13,7 @@ export class MyApp {
   }
 
   constructor(platform) {
-    this.rootPage = TabsPage;
+    this.rootPage = StorePage;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
---
+++ app/pages/store/store.js
@@ -0,0 +1,30 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the StorePage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/store/store.html',
+})
+export class StorePage {
+  static get parameters() {
+    return [[NavController]];
+  }
+
+  constructor(nav) {
+    this.nav = nav;
+    this.products = [];
+    for (let i = 1; i < 10; i++) {
+        this.products.push({
+            id: 'uuid_' + i,
+            name: 'Body Solid Dumbbells ' + i,
+            description: 'Add weight to your next workout! ' + i,
+            picture: '/img/product-picture-placeholder.png',
+            price: i
+        });
+    }
+  }
+}
