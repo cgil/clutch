a5c8067dbe546d2e608256782e6b70e5defa42ea
Carlos Gil <cgil1210@gmail.com>
2016-06-06 17:04:07 +0100

    add isProduction config

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 var ToteConfig = {
     // @if ENVIRONMENT == 'production'
     toteApiURL: 'www.totestore.com',
-    stripePublishableKey: 'pk_live_InorHF5F9AWzp1UuBM8Xz41b'
+    stripePublishableKey: 'pk_live_InorHF5F9AWzp1UuBM8Xz41b',
+    isProduction: true
     // @endif
 
     // @if ENVIRONMENT == 'development'
     toteApiURL: '127.0.0.1:5000',
-    stripePublishableKey: 'pk_test_SSTmhE8aocfnGsmEZrN9SEAM'
+    stripePublishableKey: 'pk_test_SSTmhE8aocfnGsmEZrN9SEAM',
+    isProduction: false
     // @endif
 };
