4bff4c463ee2ec360cc0ad22c90da0e2d2a5a5c8
Carlos Gil <cgil1210@gmail.com>
2016-06-09 14:39:29 +0100

    stripe test kets in prod

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var ToteConfig = {
     // @if ENVIRONMENT == 'production'
     toteApiURL: 'www.totestore.com',
-    stripePublishableKey: 'pk_live_InorHF5F9AWzp1UuBM8Xz41b',
+    // stripePublishableKey: 'pk_live_InorHF5F9AWzp1UuBM8Xz41b',
+    stripePublishableKey: 'pk_test_SSTmhE8aocfnGsmEZrN9SEAM',
     isProduction: true
     // @endif
 
